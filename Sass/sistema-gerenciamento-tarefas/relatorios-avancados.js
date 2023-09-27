d427200e72004845d36fccdb3de7e5516d866f94
RobertLlb <83178523+RobertLlb@users.noreply.github.com>
2023-09-27 12:09:18 -0300

    Update relatorios-avancados.js

--- Sass/sistema-gerenciamento-tarefas/relatorios-avancados.js
+++ Sass/sistema-gerenciamento-tarefas/relatorios-avancados.js
@@ -1,12 +1,34 @@
-// Advanced Reports Generation
+// Função para gerar relatórios avançados
+function gerarRelatoriosAvancados() {
+    // Simule a geração de relatórios avançados (substitua com sua lógica real)
+    const relatorios = [
+        {
+            titulo: 'Relatório de Vendas',
+            conteudo: 'Este é o relatório de vendas do último trimestre.'
+        },
+        {
+            titulo: 'Relatório de Desempenho',
+            conteudo: 'Este é o relatório de desempenho da equipe de vendas.'
+        }
+    ];
 
-function generateCompletedTasksReport() {
-    const completedTasks = tasks.filter((task) => task.completed);
+    const relatoriosAvancadosElement = document.getElementById('relatoriosAvancados');
 
-    console.log("Completed Tasks Report:");
-    completedTasks.forEach((task) => {
-        console.log(`- Task: ${task.title}`);
+    // Limpe qualquer conteúdo anterior
+    relatoriosAvancadosElement.innerHTML = '';
+
+    // Adicione os relatórios à página
+    relatorios.forEach((relatorio, index) => {
+        const relatorioDiv = document.createElement('div');
+        relatorioDiv.classList.add('relatorio');
+        relatorioDiv.innerHTML = `
+            <h3>${relatorio.titulo}</h3>
+            <p>${relatorio.conteudo}</p>
+        `;
+        relatoriosAvancadosElement.appendChild(relatorioDiv);
     });
 }
 
-generateCompletedTasksReport();
+// Adicione um ouvinte de evento ao botão para gerar relatórios
+const botaoGerarRelatorios = document.getElementById('gerarRelatorios');
+botaoGerarRelatorios.addEventListener('click', gerarRelatoriosAvancados);
